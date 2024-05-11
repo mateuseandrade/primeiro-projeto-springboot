c3f43d788f9350178121632b50d2458ea78de650
Mateus <mateuseandrade@gmail.com>
2024-05-11 16:24:09 -0300

    request body

--- src/main/java/br/com/mateusandrade/controller/PrimeiraController.java
+++ src/main/java/br/com/mateusandrade/controller/PrimeiraController.java
@@ -8,6 +8,10 @@ import java.util.Map;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
 
 
 
@@ -29,5 +33,13 @@ public class PrimeiraController {
   public String metodoComQueryParams2(@RequestParam Map<String, String> allParams) {
       return "O parametro com metodoComQueryParams2 é " + allParams.entrySet();
   }
-  
+
+  @PostMapping("/metodoComBodyParams")
+  public String metodoComBodyParams(@RequestBody Usuario usuario) {
+      return "metodoComBodyParams " + usuario.username();
+  }
+
+  record Usuario(String username) {
+  }
+
 }
